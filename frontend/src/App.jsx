798fb6d53d13c6ab4cb3228f99717cec834dafd4
AnshGP <ianshgp222003@gmail.com>
2025-02-22 00:19:17 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=' bg-green-500 text-center p-5'>
+      Hi
+    </div>
+    </>
+  )
+}
+
+export default App
