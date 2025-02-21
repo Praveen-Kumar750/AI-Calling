133e89cb28707773e703ec11d9595caffbcf76bb
Soyelrana <soyelrana224@gmail.com>
2025-02-22 01:08:21 +0530

    frontend v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,9 +8,7 @@ function App() {
 
   return (
     <>
-    <div className=' bg-green-500 text-center p-5'>
-      Hi
-    </div>
+      
     </>
   )
 }
